5a1ee3be7984912f44fb2299b6fcbcd57cdf1b57
kosta <kosta@DESKTOP-H68DGGS>
2017-03-20 12:43:15 +0900

    사진도메인, 인화도매인

--- totobookProj/src/totoBook/domain/Photo.java
+++ totobookProj/src/totoBook/domain/Photo.java
@@ -1,8 +1,35 @@
 package totoBook.domain;
 
-import java.util.List;
-
 public class Photo {
-	private String printId;
-	private List<Photo> photos;
+	private String photoId;
+	private String fileName;
+	private String contentType;
+	private int amount;
+	
+	public String getPhotoId() {
+		return photoId;
+	}
+	public void setPhotoId(String photoId) {
+		this.photoId = photoId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
 }
