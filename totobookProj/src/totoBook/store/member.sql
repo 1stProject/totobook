dc665f655127f532dae8afa9c89dc6ad1da60b29
kosta <kosta@DESKTOP-GFR28TA>
2017-03-20 16:15:47 +0900

    member.sql

---
+++ totobookProj/src/totoBook/store/member.sql
@@ -0,0 +1,17 @@
+--DROP TABLE MEMBER_TB;
+
+
+CREATE TABLE member_tb (
+  member_id varchar2(50),
+  name varchar2(50),
+  password varchar2(50),
+  address varchar2(50),
+  phoneNumber varchar2(50),
+  receiveEmail varchar2(50)
+);
+
+insert into member_tb(member_id, name, password, address, phoneNumber, receiveEmail) values ('user','서대현','1234','서울 금천구','010-1234-1234','true');
+insert into member_tb(member_id, name, password, address, phoneNumber, receiveEmail) values ('man','이승건','4321','서울 금천구','010-1111-1111','true');
+
+
+commit;
