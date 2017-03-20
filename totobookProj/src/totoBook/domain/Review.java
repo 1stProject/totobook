f34a7df2496d581d978d09ee21dd51b46746a725
kosta <kosta@DESKTOP-H68DGGS>
2017-03-20 15:06:26 +0900

    포토맵퍼, 프린트맵퍼, 오더맵퍼

--- totobookProj/src/totoBook/domain/Review.java
+++ totobookProj/src/totoBook/domain/Review.java
@@ -6,5 +6,24 @@ public class Review {
 	private Member member;
 	private String comment;
 	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
 	
 }
