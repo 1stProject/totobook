1101d88b58fdf7122c91326d4dfb25bece443652
kosta <kosta@DESKTOP-GFR28TA>
2017-03-20 12:47:18 +0900

    what

---
+++ totobookProj/src/totoBook/service/MemberService.java
@@ -0,0 +1,21 @@
+package totoBook.service;
+
+import java.util.List;
+
+import totoBook.domain.Member;
+
+public interface MemberService {
+
+	void registerMember(Member member);
+	Member findMemberById(String memberId);
+	void modifyMember(Member member);
+	void removeMember(String member);
+	boolean chackId(String memberId);
+	boolean checkPw(Member member);
+	List<Member> findMembersByTel(String memberTel);
+	List<Member> findMembersByName(String memberName);
+	List<Member> findAllMembers();
+	
+	
+	
+}
---
+++ totobookProj/src/totoBook/store/MemberStore.java
@@ -0,0 +1,17 @@
+package totoBook.store;
+
+import java.util.List;
+
+import totoBook.domain.Member;
+
+public interface MemberStore {
+
+	void insertMember(Member member);
+	Member selectMemberById(String memberId);
+	Member selectMember(Member member);
+	void updateMember(Member member);
+	void deleteMember(String member);
+	List<Member> selectMembersByTel(String memberTel);
+	List<Member> selectMembersByName(String memberName);
+	List<Member> selectAllmembers();
+}
