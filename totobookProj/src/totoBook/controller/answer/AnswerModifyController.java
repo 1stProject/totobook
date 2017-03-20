e02b12106c665a16dd274e7bacb94a7cf1bff711
kosta <kosta@DESKTOP-KFS0SFJ>
2017-03-20 14:31:36 +0900

    controller

--- totobookProj/src/totoBook/controller/answer/AnswerModifyController.java
+++ totobookProj/src/totoBook/controller/answer/AnswerModifyController.java
@@ -7,16 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AnswerModifyController
- */
+
 @WebServlet("/answer/modify.do")
 public class AnswerModifyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
