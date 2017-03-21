d9e27f139ad82e3da7d81c11a7bb0e9fccc23b93
lsg7626 <dltmdrjs2000@naver.com>
2017-03-21 12:09:51 +0900

    Update PrintStoreLogic.java

--- totobookProj/src/totoBook/store/logic/PrintStoreLogic.java
+++ totobookProj/src/totoBook/store/logic/PrintStoreLogic.java
@@ -1,20 +1,52 @@
 package totoBook.store.logic;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import totoBook.domain.Print;
 import totoBook.store.PrintStore;
+import totoBook.store.mapper.PrintMapper;
 
 /**
  * @author
  * @version 1.0
  */
 public class PrintStoreLogic implements PrintStore {
-
+	private SqlSessionFactory factory;
+	
+	public PrintStoreLogic() {
+		factory = SqlSessionFactoryProvider.getSqlSessionFactory();
+	}
 	@Override
 	public void insertPrint(Print print) {
+		SqlSession session = factory.openSession();
+		try {
+			PrintMapper mapper = session.getMapper(PrintMapper.class);
+			mapper.insertPrint(print);
+			session.commit();
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
-	public void deletePrint(Print print) {
+	public void deletePrint(String printId) {
+		
+	}
+	
+	@Override
+	public List<Print> selectPrintsByUserId(String userId) {
+		SqlSession session = factory.openSession();
+		List<Print> prints = null;
+		try {
+			PrintMapper mapper = session.getMapper(PrintMapper.class);
+			prints = mapper.selectPrintsByUserId(userId);
+		} finally {
+			session.close();
+		}
+		return prints;
 	}
 
 }
