27205368a0a97c3421407500943a8f88b38d9638
kosta <kosta@DESKTOP-KFS0SFJ>
2017-03-21 09:17:17 +0900

    storelogic

---
+++ totobookProj/src/totoBook/service/logic/QuestionServiceLogic.java
@@ -0,0 +1,40 @@
+package totoBook.service.logic;
+
+import java.util.List;
+
+import totoBook.domain.Post;
+import totoBook.service.QuestionService;
+
+public class QuestionServiceLogic implements QuestionService{
+
+	@Override
+	public void registerQuestion(Post post) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Post> findAllQuestion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Post findQuestionById(String questionId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modifyQuestion(Post post) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeQuestion(String questionId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ totobookProj/src/totoBook/store/logic/AnswerStoreLogic.java
@@ -0,0 +1,32 @@
+package totoBook.store.logic;
+
+import totoBook.domain.Answer;
+import totoBook.store.AnswerStore;
+
+public class AnswerStoreLogic implements AnswerStore{
+
+	@Override
+	public void insertAnswer(Answer answer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Answer selectAnswerById(String postId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateAnswer(Answer answer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAnswer(String postId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
