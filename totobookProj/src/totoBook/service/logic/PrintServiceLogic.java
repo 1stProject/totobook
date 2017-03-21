fa3d25620e9ee927be0b4cbbf9c8ce14ed329710
lsg7626 <dltmdrjs2000@naver.com>
2017-03-21 12:07:35 +0900

    Update PrintServiceLogic.java

--- totobookProj/src/totoBook/service/logic/PrintServiceLogic.java
+++ totobookProj/src/totoBook/service/logic/PrintServiceLogic.java
@@ -5,27 +5,45 @@ import java.util.List;
 import totoBook.domain.Photo;
 import totoBook.domain.Print;
 import totoBook.service.PrintService;
+import totoBook.store.PhotoStore;
+import totoBook.store.PrintStore;
+import totoBook.store.logic.PhotoStoreLogic;
+import totoBook.store.logic.PrintStoreLogic;
 
 /**
  * @author
  * @version 1.0
  */
 public class PrintServiceLogic implements PrintService {
+	PrintStore printStore = null;
+	PhotoStore photoStore = null;
+	public PrintServiceLogic() {
+		printStore = new PrintStoreLogic();
+		photoStore = new PhotoStoreLogic();
+	}
 	@Override
 	public void registerPrint(Print print) {
-		
+		printStore.insertPrint(print);
 	}
 
 	@Override
 	public void registerPhotos(List<Photo> photos) {
+		photoStore.insertPhotos(photos);
 	}
 
 	@Override
 	public void removePrint(String printId) {
+		printStore.deletePrint(printId);
 	}
 
 	@Override
-	public void removePhotos(List<Photo> photos) {
+	public void removePhotos(String printId) {
+		photoStore.deletePhotos(printId);
+	}
+
+	@Override
+	public List<Print> findPrintsByUserId(String userId) {
+		return null;
 	}
 
 }
