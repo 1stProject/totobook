f40164797331f6d95ba8b81dcde57ed6a4ef5f5f
kosta <kosta@DESKTOP-KFS0SFJ>
2017-03-21 11:45:29 +0900

    d

--- totobookProj/src/totoBook/store/logic/QuestionStoreLogic.java
+++ totobookProj/src/totoBook/store/logic/QuestionStoreLogic.java
@@ -3,16 +3,33 @@ package totoBook.store.logic;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import totoBook.domain.Post;
 import totoBook.store.QuestionStore;
+import totoBook.store.mapper.MemberMapper;
+import totoBook.store.mapper.QuestionMapper;
 
-public class QuestionStoreLogic implements QuestionStore{
+public class QuestionStoreLogic implements QuestionStore {
+
+	private SqlSessionFactory factory;
+
+	public QuestionStoreLogic() {
+		factory = SqlSessionFactoryProvider.getSqlSessionFactory();
+	}
 
 	@Override
 	public void insertQuestion(Post post) {
-		
-		
+		SqlSession session = factory.openSession();
+
+		try {
+			QuestionMapper mapper = session.getMapper(QuestionMapper.class);
+			mapper.insertQuestion(post);
+			session.commit();
+		} finally {
+			session.close();
+		}
+
 	}
 
 	@Override
@@ -30,13 +47,13 @@ public class QuestionStoreLogic implements QuestionStore{
 	@Override
 	public void updateQuestion(Post post) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteQuestion(String questionId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
