23021d489ec7e52e5772ed3cd4099e7d3d50887e
Lee <dltmdrjs2000@naver.com>
2017-03-21 15:13:23 +0900

    이승건 order
    1. orderlist.jsp

---
+++ totobookProj/src/totoBook/controller/order/OrderListController.java
@@ -0,0 +1,32 @@
+package totoBook.controller.order;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import totoBook.domain.Order;
+import totoBook.service.OrderService;
+import totoBook.service.logic.OrderServiceLogic;
+
+/**
+ * @author
+ * @version 1.0
+ */
+@WebServlet("/order/list.do")
+public class OrderListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		OrderService service = new OrderServiceLogic();
+		
+		List<Order> list = service.findAllOrders();
+		request.setAttribute("orders", list);
+		request.getRequestDispatcher("/views/order/orderList.jsp").forward(request, response);
+	}
+
+}
