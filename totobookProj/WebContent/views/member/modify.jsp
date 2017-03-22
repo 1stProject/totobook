565acac14efe2875616313fc891ecc848d9ba3c6
kosta <kosta@DESKTOP-GFR28TA>
2017-03-22 14:25:23 +0900

    대현이것저것

--- totobookProj/WebContent/views/member/modify.jsp
+++ totobookProj/WebContent/views/member/modify.jsp
@@ -5,43 +5,45 @@
 <c:set var="ctx">${pageContext.request.contextPath }</c:set>
 <html>
 <head>
-<title>회원정보</title>
+<title>회원정보수정</title>
 </head>
 <body>
-	<a href="list.do">목록화면으로</a>
-	<h3>회원정보</h3>
-	<form action="modify.do" method="post">
+	<a href="list.do">회원리스트</a>
+	<h3>회원정보수정</h3>
+	<form action="${ctx }/member/modify.do" method="post">
+			<input id="memberId" name="memberId" type="hidden"
+			value="${member.memberId}">
 		<table class="table">
 			<tr>
 				<th>아이디</th>
 				<td><input id="memberId" name="memberId" type="text"
-					value="${member.memberId}"></td>
+					value=""></td>
 			</tr>
-			<tr>
+						<tr>
 				<th>비밀번호</th>
-				<td><input id="memberPassword" name="memberPassword"  
-					type="text" value="${member.password}"></td>
-			</tr>
+				<td><input id="memberPassword" name = "memberPassword" type="password"
+						value=""></td>
+						</tr>
 			<tr>
 				<th>폰번</th>
 				<td><input id="memberPhone" name="memberPhone" 
-						value="${member.phone}"></td>
+						value=""></td>
 			</tr>
 			<tr>
 				<th>주소</th>
 				<td><input id="memberAddress" name="memberAddress" 
-						value="${member.address}"></td>
+						value=""></td>
 			</tr>
 			<tr>	
 				<th>이메일</th>
-				<td><input id="memberEmail" name="memberEmail"  
-						value="${member.receiveEmail}"></td>
+				<td><div class = "radioc"><input type = "radio" name = "memberEmail" value = "yes"> 유
+				<input type = "radio" name = "memberEmail" value = "no"> 무
+				</div></td>
 			</tr>			
 		</table>
 		<div align="center">
-		<input class="btn" type="reset" value="취소"> 
-		<input class="btn" type="submit"
-			value="저장"></div>
+		<input type="reset" value="취소"> 
+		<input type="submit" value="저장"></div>
 	</form>
 	<br>
 </body>
--- totobookProj/src/totoBook/controller/member/MemberModifyController.java
+++ totobookProj/src/totoBook/controller/member/MemberModifyController.java
@@ -1,13 +1,12 @@
 package totoBook.controller.member;
 
-import java.io.IOException;
+import java.io.IOException; 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.glass.ui.Size;
 
 import totoBook.domain.Member;
 import totoBook.service.MemberService;
@@ -16,10 +15,10 @@ import totoBook.service.logic.MemberServiceLogic;
 @WebServlet("/member/modify.do")
 public class MemberModifyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	MemberService service = new MemberServiceLogic();
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberService service = new MemberServiceLogic();
 		String id = request.getParameter("memberId");
 		Member member = service.findMemberById(id);
 		request.setAttribute("member", member);
@@ -28,16 +27,16 @@ public class MemberModifyController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String id = request.getParameter("memberId");
-		String password = request.getParameter("memberpassword");
 		String phone = request.getParameter("memberPhone");
+		String password = request.getParameter("memberPassword");
 		String address = request.getParameter("memberAddress");
 		String email = request.getParameter("memberEmail");
 		
-		
+		MemberService service = new MemberServiceLogic();
 		Member member = new Member();
 		member.setMemberId(id);
-		member.setPassword(password);
 		member.setPhone(phone);
+		member.setPassword(password);
 		member.setAddress(address);
 		member.setReceiveEmail(email);
 		service.modifyMember(member);
