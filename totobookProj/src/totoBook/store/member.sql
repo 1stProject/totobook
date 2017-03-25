895056fdab285b3076d878d54973bd14659b0f2f
kosta <kosta@DESKTOP-GFR28TA>
2017-03-25 10:33:45 +0900

    디비

--- totobookProj/src/totoBook/store/member.sql
+++ totobookProj/src/totoBook/store/member.sql
@@ -10,8 +10,9 @@ CREATE TABLE member_tb (
   PRIMARY KEY (member_id)
 );
 
-insert into member_tb(member_id, name, password, address, phone, receive_Email) values ('RURE1114','김주희','1234','서울시 구로구','010-1234-1234','true');
-insert into member_tb(member_id, name, password, address, phone, receive_Email) values ('Lee@Lee.com','이승건','4321','서울시 광진구','010-1111-1111','true');
+insert into member_tb(member_id, name, password, address, phone, receive_Email) values ('RURE1114','김주희','1234','서울시 구로구','010-1234-1234','yes');
+insert into member_tb(member_id, name, password, address, phone, receive_Email) values ('Lee@Lee.com','이승건','4321','서울시 광진구','010-1111-1111','no');
+insert into member_tb(member_id, name, password, address, phone, receive_Email) values ('admin','admin','admin','admin','admin','yes');
 
 
 commit;
