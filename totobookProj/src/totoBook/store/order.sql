0f9feaa086cfed4b515949c837e75bf9b4e10d40
lsg7626 <dltmdrjs2000@naver.com>
2017-03-26 11:47:44 +0900

    이승건 order.sql

--- totobookProj/src/totoBook/store/order.sql
+++ totobookProj/src/totoBook/store/order.sql
@@ -17,7 +17,8 @@ CREATE TABLE order_tb (
   payment varchar2(50), 
   total_price NUMBER,
   address varchar2(100),
-  product_id varchar2(50),
+  bookphoto_Id varchar2(50),
+  category varchar2(50),
   PRIMARY KEY (order_id)
 );
 
@@ -36,10 +37,6 @@ CREATE TABLE print_tb (
 ALTER TABLE print_tb
 ADD CONSTRAINT print_tb_fk1 FOREIGN KEY (member_id)
 REFERENCES member_tb (member_id);
-
-alter table order_tb add(category VARCHAR2(50));
-
-ALTER table order_tb rename column product_id to bookphoto_id;
 
 insert into print_tb (print_id, member_id, product_id, print_option) 
 values (print_seq.NEXTVAL,'Lee@Lee.com', 2,'8*8');
@@ -69,4 +66,7 @@ values (photo_seq.NEXTVAL, '/images/3.jpg', 2, 1);
 
 insert into order_tb (order_id, member_id, order_date, payment, total_price, bookphoto_id, category) 
 values (order_seq.NEXTVAL,'Lee@Lee.com','2017-3-20','신용카드', '150000', '1', '사진');
+
+insert into order_tb (order_id, member_id, order_date, payment, total_price, bookphoto_id, category) 
+values (order_seq.NEXTVAL,'RURE1114','2017-3-20','현금', '50000', '1', '포토북');
 commit;
